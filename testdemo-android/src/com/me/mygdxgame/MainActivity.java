8cb65b8a57822f2efa60d64e294538169d5e3f04
songjunfeng <303154453@qq.com>
2014-11-12 17:37:20 +0800

    android平台分辨率自动匹配

--- testdemo-android/src/com/me/mygdxgame/MainActivity.java
+++ testdemo-android/src/com/me/mygdxgame/MainActivity.java
@@ -1,25 +1,27 @@
 package com.me.mygdxgame;
 
 import android.os.Bundle;
+import android.view.Display;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.blackvine.constant.Constants;
 
 public class MainActivity extends AndroidApplication {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        cfg.useGL20 = true;
-        
-        initialize(new MyGdxGame(), cfg);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Display mDisplay = getWindowManager().getDefaultDisplay();
+		Constants.SCREEN_WIDTH = mDisplay.getWidth();
+		Constants.SCREEN_HEIGHT = mDisplay.getHeight();
+
+		AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+		cfg.useGL20 = true;
+		initialize(new MyGdxGame(), cfg);
+	}
 
 	@Override
 	public void onBackPressed() {
-		// TODO Auto-generated method stub
 	}
 
-    
 }
